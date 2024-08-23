f1a74b6908cc6774a05ed168c50b8897ee3a4ae1
Saba Madadi <sabamadadi9@gmail.com>
2024-08-24 00:59:32 +0330

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int c = (int) scanner.nextFloat();
+        int d = (int) scanner.nextFloat();
+        System.out.println(d * c);
+        System.out.println(c + d);
+
+    }
+}
---
+++ Even & Odd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+        String s3 = scanner.nextLine();
+        s1 = s1.toUpperCase();
+        s2 = s2.toLowerCase();
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3.charAt(3));
+    }
+}
