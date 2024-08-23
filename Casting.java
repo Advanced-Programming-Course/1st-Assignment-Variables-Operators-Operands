2b8316a3b3823433655cf83e4f0a65fcaa678261
Saba Madadi <sabamadadi9@gmail.com>
2024-08-24 01:01:09 +0330

    Update Casting.java

--- Casting.java
+++ Casting.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Casting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int c = (int) scanner.nextFloat();
