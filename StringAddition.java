f15a5896c5094f32d69f460c935430fd5039d7c2
Saba Madadi <sabamadadi9@gmail.com>
2024-08-24 15:25:41 +0330

    Update StringAddition.java

--- StringAddition.java
+++ StringAddition.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class Main{
+public class StringAddition{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s1 = scanner.nextLine();
